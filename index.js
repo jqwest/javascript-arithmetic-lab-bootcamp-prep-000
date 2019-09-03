336007d5f21ac9c3c6d0a6492a03d26fc64b30a7
john L <johnleavell@gmail.com>
2019-09-03 03:21:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,19 +15,19 @@ function divide(a, b){ //divide a and b and return the result.
   number = a / b;
   return number;
 }
-function inc(a){ //Increment a by 1 and return the result.
-  number = a + 1;
+function inc(n){ //Increment a by 1 and return the result.
+  number = n += 1;
   return number;
 }
-function dec(a){ //Decrement a by 1 and return the result.
-  number = a - 1;
+function dec(n){ //Decrement a by 1 and return the result.
+  number = n -= 1;
   return number;
 }
-function makeInt(a){ //Parse a as an integer and return that integer.
-  number = parseInt(a, 10);
+function makeInt(n){ //Parse a as an integer and return that integer.
+  number = parseInt(n, 10);
   return number;
 }
-function preserveDecimal(a){ //Parse a as a float (number with decimals) and return that number.
-  number = parseFloat(a, 10);
+function preserveDecimal(n){ //Parse a as a float (number with decimals) and return that number.
+  number = parseFloat(n, 10);
   return number;
 }
